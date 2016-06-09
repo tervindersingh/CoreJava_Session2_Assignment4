f6a00be8e64f54c42db3faec32f825d2c96ac67b
tervindersingh <tervindersingh.2005@gmail.com>
2016-06-09 04:03:30 +0000

    To print number of days in a given month

---
+++ PrintDays.java
@@ -0,0 +1,49 @@
+public class PrintDays
+{
+	public static void main(String args[])
+	{
+		int a=5;
+
+		switch (a)
+		{
+		case 1:
+		System.out.println("This is January and there are 31 days in the month");
+		break;
+		case 2:
+		System.out.println("This is Feb and there are either 28 or 29 days in the month");
+		break;
+		case 3:
+		System.out.println("This is March and there are 31 days in the month");
+		break;
+		case 4:
+		System.out.println("This is Apr and there are 30 days in the month");
+		break;
+		case 5:
+		System.out.println("This is May and there are 31 days in the month");
+		break;
+		case 6:
+		System.out.println("This is Jun and there are 30 days in the month");
+		break;
+		case 7:
+		System.out.println("This is Jul and there are 31 days in the month");
+		break;
+		case 8:
+		System.out.println("This is Aug and there are 31 days in the month");
+		break;
+		case 9:
+		System.out.println("This is Sept and there are 30 days in the month");
+		break;
+		case 10:
+		System.out.println("This is Oct and there are 31 days in the month");
+		break;
+		case 11:
+		System.out.println("This is Nov and there are 30 days in the month");
+		break;
+		case 12:
+		System.out.println("This is Dec and there are 31 days in the month");
+		break;
+		default:
+		System.out.println("You entered a wrong input:");
+		}
+	}
+}
